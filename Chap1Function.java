3f5e849c51c92334b67d29acec8ddb23ef75f361
OliPavo <52759113+OliPavo@users.noreply.github.com>
2019-09-21 11:02:42 -0400

    Add files via upload

---
+++ Chap1Function.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Chap1Function {
+
+    public static void main(String[] args) {
+        // Stores the user's input
+        Scanner input = new Scanner(System.in);
+
+        while(true) {
+            System.out.println("Please enter an integer to evaluate or type 'quit' to end:");
+            String answer = input.nextLine();
+            // Checks if user entered 'quit'
+            if(answer.equals("quit")) {
+                break;
+            }
+            // Checks if user entered an integer
+            try{
+                int number = Integer.parseInt(answer);
+                int [] output;
+
+                output = simpleMethod(number);
+                System.out.println("Here's your output: " + Arrays.toString(output));
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please try again.");
+            }
+        }
+
+    }
+    // Evaluates the input as specified by the given algorithm
+    public static int [] simpleMethod(int number){
+        ArrayList<Integer> outputs = new ArrayList<>();
+
+        do {
+            boolean isEven;
+            // Checks if integer is even
+            if (number % 2 == 0) {
+                isEven = true;
+            } else {
+                isEven = false;
+            }
+            // Evaluates integer and returns number for output
+            if (number == 1) {
+            } else if (isEven) {
+                number = number / 2;
+            } else {
+                number = (number * 3) + 1;
+            }
+            outputs.add(number);
+        }while(number != 1);
+        //Converts the interger arrayList to an integer array
+        int[] outputArray = outputs.stream().mapToInt(i -> i).toArray();
+        return outputArray;
+    }
+}
